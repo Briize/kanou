752ecdb128e12c6bf3303caf9b173b41167d7735
James Redfern <jamesr@xigen.co.uk>
2017-04-13 17:28:48 +0100

    file setup

---
+++ header.php
@@ -0,0 +1,15 @@
+<head>
+
+  	<title>Nightjar</title>
+  	<meta charset="UTF-8">
+	<meta name="description" content="Nightjar Survival">
+	<meta name="keywords" content="HTML,CSS,PHP,JavaScript">
+	<meta name="robots" content="index, follow"/>
+	<meta name="author" content="Briize">
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, minimum-scale=1, user-scalable=no">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+
+  	<!--[if lt IE 9]>
+    	<script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.js"></script>
+  	<![endif]-->
+</head>
