88da6b145852b56ccaa985f80a68e482d3014ee4
Briize <jamesdavidredfern@gmail.com>
2017-04-26 22:11:30 +0100

    Additions made to add-combatants

--- add-combatants.php
+++ add-combatants.php
@@ -8,7 +8,31 @@
 
 			<div class="content">
 		
-				
+				<div id="combatant_1" class="combatant">
+
+					<div class="remove"></div>
+					
+					<div class="combatant_image">
+						
+						<div class="combatant_image_container">
+							
+							<img src="http://i.imgur.com/N8V2ahs.jpg">
+
+						</div>
+
+						<div class="combatant_image_inputs">
+							
+							<input class="combatant_image_inputs_input" type="text" name="combatant_image_input_1" placeholder="Image URL">
+							<button class="combatant_image_inputs_button" type="button">Update Image</button>
+
+						</div>						
+
+
+					</div>
+
+
+
+				</div>
 
 			</div>
 
